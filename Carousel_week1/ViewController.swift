ec1c6cd4ed9dbae55f82f28aa320dd57d19afcdb
Ruthia He <rujiahe@gmail.com>
2015-10-27 10:02:38 -0700

    Initial Commit

---
+++ Carousel_week1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Carousel_week1
+//
+//  Created by Rujia He on 10/27/15.
+//  Copyright (c) 2015 SUNLIGHT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
